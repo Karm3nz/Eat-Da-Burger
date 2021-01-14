b50060a4e241d45a94d3be7f9e81c34d49ea384a
Carmen Hui <karmenzkarmenz@gmail.com>
2021-01-14 16:42:15 -0500

    Modified config, controller, and model folders

--- db/schema.sql
+++ db/schema.sql
@@ -11,9 +11,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   burger_name VARCHAR(255) NOT NULL,
-  devoured BOOLEAN
+  devoured BOOLEAN DEFAULT false
 );
 
 -- INSERT INTO burgers (burger_name, devoured)
